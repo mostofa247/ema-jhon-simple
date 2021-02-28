3d930545f7759761e71f41c24d5c878748eb655f
mostofa247 <74599998+mostofa247@users.noreply.github.com>
2021-02-28 17:23:23 -0500

    shop update

---
+++ src/components/Header/Shop/Shop.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useState } from 'react';
+import fakeData from '../../../fakeData';
+import './Shop.css';
+
+
+const Shop = () => {
+    const first10 = fakeData.slice(0, 10);
+    const [products, setProducts] = useState(first10);
+    return (
+        <div className="shop-container">
+            <div className="product-container">
+                <ul>
+                    {
+                        products.map(product => <li>{product.name}</li>)
+                    }
+                </ul>
+            </div>
+            <div className="cart-container">
+                <h1>Shopping cart</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
